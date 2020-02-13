d910320bf949fc69a513effa257aa493e424047a
alexisOclock <alexis@oclock.io>
2020-02-13 09:40:07 +0100

    année dynamique dans footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const Footer = () => (
-  <footer>
-    <p>DevOfThrones, le blog du développeur React 2020 &copy;</p>
-  </footer>
-);
+const Footer = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  return (
+    <footer>
+      <p>DevOfThrones, le blog du développeur React {year} &copy;</p>
+    </footer>
+  );
+};
 
 export default Footer;
+
+// // a propos des fléchées
+// // on écrit le corps de la fonction entre { }
+// // on peut écrire des instructions puis retourner quelque chose
+// const addition = () => {
+//   const a = 3;
+//   const b = 5;
+//   const result = a + b;
+//   return result;
+// };
+
+// // dans le cas où on retourne directement, on peut l'écrire sans rien changer avec un return explicite
+// const addition = () => {
+//   return 3 + 5;
+// }
+// // on peut aussi rendre le return implicite, en ne l'écrivant pas et en n'écrivant pas les { }
+// const addition = () => 3 + 5;
+// // on peut aussi si on veut, entourer le retour avec des parenthèses pour la lisibilité, pratique lorsqu'on retourne du jsx sur plusieus lignes
+// const addition = () => (
+// 3 + 5
+// );
