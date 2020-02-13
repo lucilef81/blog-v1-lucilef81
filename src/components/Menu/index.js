e6b39d85262e8167b363b674cd9f1ba502e06009
alexisOclock <alexis@oclock.io>
2020-02-13 13:40:16 +0100

    découverte des event

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { element } from 'prop-types';
 
 import MenuStyled from './MenuStyled';
 
 // j'ai ["Angular", "React", "O’clock", "Autre"]
 // je veux [<a>Angular</a>,<a>React</a>,<a>Oclock</a>,<a>Autre</a>]
 
+// https://fr.reactjs.org/docs/events.html
+
 const Menu = ({ categories }) => (
   <MenuStyled>
     <nav>
-      {categories.map((category) => <a key={category}>{category}</a>)}
+      {categories.map((category) => (
+        <a
+          className="truc"
+          // on attache l'événement directement sur la description jsx de notre élement
+          // on associe une fonction de rappel qui sera executée quand l'événement aura lieu
+          // comme à notre habitude on peut récupérer en paramètre un objet représentant l'événement
+          onClick={(event) => {
+            console.log('click', event.target);
+          }}
+          key={category}
+        >
+          {category}
+        </a>
+      ))}
     </nav>
   </MenuStyled>
 );
