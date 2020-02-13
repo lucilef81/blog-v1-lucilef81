6fa851c35d8a0665e49a632d5e3aaf169c68ec5b
alexisOclock <alexis@oclock.io>
2020-02-13 09:16:39 +0100

    découpage

---
+++ src/components/Blog/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => (
+  <div id="container">
+    <div>Menu</div>
+    <div>Articles</div>
+    <div>Footer</div>
+  </div>
+);
+
+export default Blog;
