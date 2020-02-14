683d0394bb151694b8112948df693c6cd99d9604
alexisOclock <alexis@oclock.io>
2020-02-14 10:19:13 +0100

    composant prépapré

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -5,16 +5,26 @@ import Menu from 'src/components/Menu';
 import Articles from 'src/components/Articles';
 
 import categories from 'src/data/categories';
-import posts from 'src/data/posts';
+// import posts from 'src/data/posts';
 
 import BlogStyled from './BlogStyled';
 
 const Blog = () => {
   const [currentCategory, setCurrentCategory] = useState('Accueil');
+  const [posts, setPosts] = useState([]);
 
   let selectedPosts = posts;
   if (currentCategory !== 'Accueil') {
     selectedPosts = posts.filter((post) => post.category === currentCategory);
+  }
+
+  // on peut préparer un composant intermédiaire
+  let Welcome;
+  if (posts.length === 0) {
+    Welcome = () => <button type="button">Cliquez pour charger</button>;
+  }
+  else {
+    Welcome = () => <Articles category={currentCategory} articles={selectedPosts} />;
   }
 
   return (
@@ -24,7 +34,7 @@ const Blog = () => {
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
       />
-      <Articles category={currentCategory} articles={selectedPosts} />
+      <Welcome />
       <Footer />
     </BlogStyled>
   );
